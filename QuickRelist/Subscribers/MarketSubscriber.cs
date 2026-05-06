af6973335719f550eeade75ae511402480c81f6b
oorzkws <65210810+oorzkws@users.noreply.github.com>
2026-05-06 13:46:00 -0600

    Refactor MarketSubscriber to use events
    Much nicer to parse, I think

--- QuickRelist/Subscribers/MarketSubscriber.cs
+++ QuickRelist/Subscribers/MarketSubscriber.cs
@@ -10,26 +10,23 @@ using FFXIVClientStructs.FFXIV.Client.UI.Info;
 using QuickRelist.Extensions;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.Linq;
 using static ECommons.Throttlers.EzThrottler;
 using static QuickRelist.QuickRelist;
+using SortedHistory = System.Collections.Generic.SortedSet<Dalamud.Game.Network.Structures.IMarketBoardHistoryListing>;
+using SortedListings = System.Collections.Generic.SortedSet<Dalamud.Game.Network.Structures.IMarketBoardItemListing>;
+
 
 namespace QuickRelist;
 
 public class MarketSubscriber : IDisposable {
     public readonly HashSet<uint> CachedItems = new();
-    public readonly ConcurrentDictionary<uint, SortedSet<IMarketBoardHistoryListing>> ItemSalesHistory = new();
-    public readonly ConcurrentDictionary<uint, SortedSet<IMarketBoardItemListing>> ItemCurrentOfferings = new();
+    public readonly ConcurrentDictionary<uint, SortedHistory> ItemSalesHistory = new();
+    public readonly ConcurrentDictionary<uint, SortedListings> ItemCurrentOfferings = new();
     public readonly Queue<uint> RequestQueue = new();
 
     public uint ExpectedOfferingsParts { get; private set; }
     public uint ReceivedOfferingsParts { get; private set; }
-    
-    public delegate void OnRequestUpdatedDelegate(IMarketBoardHistory? history, IMarketBoardCurrentOfferings? listings);
-    public delegate void OnRequestStartedDelegate(uint totalPackets);
-    public delegate void OnRequestErroredDelegate(uint exceptionCode);
-    public event OnRequestUpdatedDelegate OnRequestUpdated;
-    public event OnRequestStartedDelegate OnRequestStarted;
-    public event OnRequestErroredDelegate OnRequestErrored;
 
     private TaskManager MarketTaskManager { get; set; }
 
@@ -50,16 +47,9 @@ public class MarketSubscriber : IDisposable {
         }
     }
 
-    public void Stub(){}
-
-
     public unsafe MarketSubscriber() {
         requestDataHook ??= Hook.HookFromAddress<InfoProxyItemSearch.Delegates.RequestData>((nint)InfoProxyItemSearch.StaticVirtualTablePointer->RequestData, RequestDataDetour);
         requestDataHook?.Enable();
-
-        OnRequestStarted += _ => {};
-        OnRequestUpdated += (_, _) => {};
-        OnRequestErrored += _ => {};
         
         IMarketBoard.HistoryReceived += OnHistoryReceived;
         IMarketBoard.OfferingsReceived += OnOfferingsReceived;
@@ -91,39 +81,67 @@ public class MarketSubscriber : IDisposable {
         Log.Verbose($"Intercepting search for ItemId {LastRequestedItemId}");
         return requestDataHook!.Original(self);
     }
+    
+    public delegate void OnRequestFinishedDelegate(MarketSubscriber self, uint itemId, SortedListings itemListings, SortedHistory historyListings);
+    public event OnRequestFinishedDelegate OnRequestFinished = (_, _, _, _) => {};
+    
+    public delegate void OnRequestErroredDelegate(MarketSubscriber self, uint itemId, uint exceptionCode);
+    public event OnRequestErroredDelegate OnRequestErrored = delegate(MarketSubscriber self, uint itemId, uint statusCode) {
+        var errorHex = $"0x{statusCode:X8}";
+        Log.Warning($"Server declined our market request, status code {errorHex}");
+        // Place the request back at the top of the queue if it was a rate-limit error
+        if (errorHex == "0x70000003")
+            self.EnqueueRequest(itemId, true);
+        else { // otherwise give up
+            Log.Warning("Received non-ratelimit error, giving up");
+            self.IsBusy = false;
+        }
+    };
+    
+    public delegate void OnRequestUpdatedDelegate(MarketSubscriber self, uint itemId, uint expectedPackets, uint receivedPackets);
+    public event OnRequestUpdatedDelegate OnRequestUpdated = delegate (MarketSubscriber self, uint itemId, uint expectedPackets, uint receivedPackets) {
+        // This will also trigger if there are 0 offerings and the history has arrived
+        if (receivedPackets >= expectedPackets) {
+            Log.Verbose($"Finished receiving {receivedPackets} packets of listings for ItemId {itemId}");
+            self.CachedItems.Add(itemId);
+            self.OnRequestFinished.Invoke(self, itemId, self.ItemCurrentOfferings[itemId], self.ItemSalesHistory[itemId]);
+            self.IsBusy = false;
+        }
+    };
+    
+    public delegate void OnRequestStartedDelegate(MarketSubscriber self, uint itemId);
+    public event OnRequestStartedDelegate OnRequestStarted = delegate(MarketSubscriber self, uint itemId) {
+        self.ItemCurrentOfferings[itemId] = new SortedListings(new ItemListingsByPrice());
+        self.ItemSalesHistory[itemId] = new SortedHistory(new HistoryListingsByPrice());
+        self.CachedItems.Remove(itemId);
+    };
 
+    private void CacheItemHistory(uint itemId, IReadOnlyList<IMarketBoardHistoryListing> historyListings) {
+        if (!ItemSalesHistory.ContainsKey(itemId))
+            ItemSalesHistory[itemId] = new SortedHistory(new HistoryListingsByPrice());
+        foreach (var listing in historyListings)
+            ItemSalesHistory[itemId].Add(listing);
+    }
+    
     private void OnHistoryReceived(IMarketBoardHistory sales) {
-        var itemId = LastRequestedItemId;
-        Log.Verbose($"Received History for {itemId}");
-        if (ItemSalesHistory.TryGetValue(itemId, out var cachedHistory)) {
-            sales.HistoryListings.Each(listing => {
-                cachedHistory.Add(listing);
-            });
-        } else {
-            Log.Verbose("Received MB sales history data with no cache structure to put it in");
-        }
-        // 0 listings and history is done, mark listing request as complete
-        if (ExpectedOfferingsParts == 0) {
-            CachedItems.Add(itemId);
-        }
+        Log.Verbose($"Received History for {sales.ItemId}");
+        CacheItemHistory(sales.ItemId, sales.HistoryListings);
+        OnRequestUpdated.Invoke(this, sales.ItemId, ExpectedOfferingsParts, ReceivedOfferingsParts);
+    }
+    
+    private void CacheItemOffering(uint itemId, IReadOnlyList<IMarketBoardItemListing> itemListings) {
+        if (!ItemCurrentOfferings.ContainsKey(itemId))
+            ItemCurrentOfferings[itemId] = new SortedListings(new ItemListingsByPrice());
+        foreach (var listing in itemListings)
+            ItemCurrentOfferings[itemId].Add(listing);
     }
 
     private void OnOfferingsReceived(IMarketBoardCurrentOfferings offerings) {
         var itemId = LastRequestedItemId;
-        if (ItemCurrentOfferings.TryGetValue(itemId, out var cachedListings)) {
-            offerings.ItemListings.Each(listing => {
-                cachedListings.Add(listing);
-            });
-        } else {
-            Log.Verbose("Received MB sales offering data with no cache structure to put it in");
-        }
-        // Handle if we're finished a multi-packet request
-        if (++ReceivedOfferingsParts >= ExpectedOfferingsParts) {
-            Log.Verbose($"Finished receiving {ExpectedOfferingsParts} packets of listings for ItemId {itemId}");
-            // Flag as cached
-            CachedItems.Add(itemId);
-            IsBusy = false;
-        }
+        Log.Verbose($"Received partial Offerings for {itemId}");
+        CacheItemOffering(itemId, offerings.ItemListings);
+        ReceivedOfferingsParts++;
+        OnRequestUpdated.Invoke(this, itemId, ExpectedOfferingsParts, ReceivedOfferingsParts);
     }
 
     private void OnConditionChange(ConditionFlag newCondition, bool bActive) {
@@ -139,31 +157,20 @@ public class MarketSubscriber : IDisposable {
     private const float listingsPerPacket = 10f;
 
     private unsafe void MarketItemRequestStartDetour(uint targetId, IntPtr packetRef) {
+        if (IsBusy) {
+            Log.Warning("Market request started before previous request finished");
+            IsBusy = false;
+        }
         try {
             // Store the amount of packets we expect to receive
             var requestData = MarketBoardItemRequest.Read(packetRef);
             // Invoke the event
             var packetsToReceive = (uint)(requestData.AmountToArrive == 0 ? 0 : float.Ceiling(requestData.AmountToArrive / listingsPerPacket));
-            OnRequestStarted.Invoke(packetsToReceive);
-            // Are we already busy?
-            if (IsBusy) {
-                Log.Warning("Market request started before previous request finished");
-            }
-            if (!requestData.Ok) {
-                // Rate-limit: 0x70000003
-                var errorId = $"0x{requestData.Status:X8}";
-                Log.Warning($"Server declined our market request, status code {errorId}");
-                // Place the request back at the top of the queue, if it was a rate-limit
-                if (errorId == "0x70000003")
-                    EnqueueRequest(LastRequestedItemId, true);
-                else {
-                    Log.Warning("Received non-ratelimit error, giving up");
-                    IsBusy = false;
-                }
-            } else {
-                Log.Verbose($"Request made for {requestData.AmountToArrive} listings");
-                ExpectedOfferingsParts = packetsToReceive;
-            }
+            ExpectedOfferingsParts = packetsToReceive;
+            Log.Verbose($"Request made for {requestData.AmountToArrive} listings");
+            OnRequestStarted.Invoke(this, targetId);
+            if (!requestData.Ok)
+                OnRequestErrored.Invoke(this, targetId, requestData.Status);
         } catch (Exception e) {
             Log.Error(e, "Error in MarketItemRequestStartDetour");
             IsBusy = false;
@@ -199,15 +206,13 @@ public class MarketSubscriber : IDisposable {
         // Clear the last search data and our cache then update the SearchItemId
         var itemId = RequestQueue.Peek();
         proxyInstance->EntryCount = 0; // Same as ClearData() according to ClientStructs
-        ItemSalesHistory[itemId] = new SortedSet<IMarketBoardHistoryListing>(new HistoryListingsByPrice());
-        ItemCurrentOfferings[itemId] = new SortedSet<IMarketBoardItemListing>(new ItemListingsByPrice());
-        CachedItems.Remove(itemId);
         proxyInstance->SearchItemId = itemId;
         // I've never seen this request return false, even on failure, but better safe than sorry
         var success = proxyInstance->RequestData();
         if (success) {
             RequestQueue.Dequeue();
         } else {
+            Log.Warning("RequestData returned false!");
             return false;
         }
         // Continue processing queue if necessary
